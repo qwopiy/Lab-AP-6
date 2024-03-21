29a8a8bbeb60e8c2c204d00d95228df1ab579af1
qwopiy <bintangkhaliqq@gmail.com>
2024-03-21 14:36:07 +0700

    entah

---
+++ Pertemuan_3/condition.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    int a;
+    cout << "Masukkan nilai = "; cin>> a;
+    
+    // //if Statement
+    // if (a < 65) {
+    //     cout << "tidak lulus" << endl;
+    // }
+    
+    //if-else Statement
+    // if (a < 65) {
+    //     cout << "tidak lulus" << endl;
+    // } else {
+    //     cout << "lulus" << endl;
+    // }
+
+    //if-else-if Statement
+    // if (a < 65) {
+    //     cout << "tidak lulus" << endl;
+    // } else if (a > 65) {
+    //     cout << "lulus" << endl;
+    // } else {
+    //     cout << "nilai pas-pasan" << endl
+    // }
+    
+    
+}
